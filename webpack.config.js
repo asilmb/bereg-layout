1e362dc13c838b9a7bf7b1bddd9f7cbef843d4cb
a.mubarak <a.mubarak@platform.live>
2021-08-31 17:22:39 +0200

    add bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -49,6 +49,27 @@ module.exports = {
                         ]
                     }
                 }
+            },
+            {
+                test: /\.(scss)$/,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader"
+                }, {
+                    loader: "postcss-loader",
+                    options: {
+                        postcssOptions: {
+                            plugins: function () {
+                                return [
+                                    require("autoprefixer")
+                                ];
+                            }
+                        }
+                    }
+                }, {
+                    loader: "sass-loader"
+                }]
             }
         ]
     },
